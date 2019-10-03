5809916db03df7eaf68cf045fb6713e3848f307b
Jason Campbell <me@jcampbell.co>
2019-10-02 20:49:10 -0600

    Add people table via migration and populate with db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,35 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('people')->insert([
+            'emails' => json_encode([
+                'codyduder@causelabs.com',
+                'lindaladee@causelabs.com',
+            ]),
+            'people' => json_encode([
+                [
+                    'first_name' => 'ladee',
+                    'last_name'  => 'linter',
+                    'age'        => 99,
+                    'email'      => 'lindaladee@causelabs.com',
+                    'secret'     => 'cmVzb3VyY2UgdmlvbGF0aW9u',
+                ],
+                [
+                    'first_name' => 'cody',
+                    'last_name'  => 'duder',
+                    'name'       => 'cody duder',
+                    'age'        => '38',
+                    'email'      => 'codyduder@causelabs.com',
+                    'secret'     => 'VXNlIHRoaXMgc2VjcmV0IHBocmFzZSBzb21ld2hlcmUgaW4geW91ciBjb2RlJ3MgY29tbWVudHM=',
+                ],
+            ]),
+            'request_ip' => '127.0.0.1',
+        ]);
     }
 }
